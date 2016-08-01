d0fb30bba1f61748dadc6f6f1db4bf7d42897fa9
Matt McManus <matt@mcmanus.io>
2016-08-01 15:43:20 -0400

    npm hyperprep removed for now

--- hyperprep.sh
+++ hyperprep.sh
@@ -2,14 +2,14 @@
 
 dotfiles_location="$HOME/.dotfiles/hypergensis"
 
-mkdir -p $
+mkdir -p $dotfiles_location
 
 brew tap > $dotfiles_location/homebrew-taps.txt
 brew ls | grep -v brew-cask > $dotfiles_location/homebrew-installs.txt
 brew cask list > $dotfiles_location/homebrew-cask-installs.txt
 ls -1 /Applications/ > $dotfiles_location/applications-reference.txt
 
-ls -1 $HOME/.nvm/current/bin/ | grep -v node| grep -v npm > $dotfiles_location/npm-global-installs.txt
+# ls -1 $HOME/.nvm/versions/node/`nvm current`/bin | grep -v node| grep -v npm > $dotfiles_location/npm-global-installs.txt
 
 echo '===================================================================='
 echo '       Dont forget to commit and push your dotfiles'
